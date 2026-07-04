fde8e8c705e9fce5cf84d330dd4b9de066a27723
Sahil kumar <sk2522136@gmail.com>
2026-07-05 03:03:39 +0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ import startScheduleBackup from './utils/scheduleBackup.js';
 
 
 
+
 import cors from 'cors';
 
 
@@ -33,7 +34,7 @@ const server = http.createServer(app);
 //socket io setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"]
   }
 });
